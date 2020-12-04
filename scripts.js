9a24ca85be72c17fb21c4f2dab4b18e6645f35c9
hvzy <hadimirza1@gmail.com>
2020-12-04 17:04:03 -0500

    Add spin function

--- scripts.js
+++ scripts.js
@@ -1,86 +1,24 @@
+let wheel = document.getElementById("wheel");
+let spin = document.getElementById("spin");
+let container = document.getElementById("container")
 
-  let numOrder = [
-    0,
-    32,
-    15,
-    19,
-    4,
-    21,
-    2,
-    25,
-    17,
-    34,
-    6,
-    27,
-    13,
-    36,
-    11,
-    30,
-    8,
-    23,
-    10,
-    5,
-    24,
-    16,
-    33,
-    1,
-    20,
-    14,
-    31,
-    9,
-    22,
-    18,
-    29,
-    7,
-    28,
-    12,
-    35,
-    3,
-    26
-  ];
-  let numRed = [
-    32,
-    19,
-    21,
-    25,
-    34,
-    27,
-    36,
-    30,
-    23,
-    5,
-    16,
-    1,
-    14,
-    9,
-    18,
-    7,
-    12,
-    3
-  ];
-  let numBlack = [
-    15,
-    4,
-    2,
-    17,
-    6,
-    13,
-    11,
-    8,
-    10,
-    24,
-    33,
-    20,
-    31,
-    22,
-    29,
-    28,
-    35,
-    26
-  ];
-  var numgreen = [0];
+spin.addEventListener("click", spinWheel)
 
-  function circularText(txt, radius, classIndex) {
+function spinWheel() {
+    // container.classList.add("spin")
+    container.style.transform = "rotate(1080deg)"
+    container.style.transition = "4s"
+    setTimeout(function() {
+        container.style.transform = "none"
+        container.style.transition = "none"
+    }, 4000);
+}
+
+function colors() {
+    
+}
+
+function circularText(txt, radius, classIndex) {
     txt = txt.split(""),
       classIndex = document.getElementsByClassName("circTxt")[classIndex];
   
